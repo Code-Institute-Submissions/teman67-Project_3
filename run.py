b9d73bd41ce39d0353c8e3525d1fc27b3435d4ad
teman67 <>
2023-10-08 10:16:23 +0000

    add welcome_page function

--- run.py
+++ run.py
@@ -1,7 +1,39 @@
 import time
+import os
 
+def welcome_page():
+    while True:
+        print("\n" + "*" * 50)
+        print("""
+        __        __   _                            _          _   _          
+        \ \      / /__| | ___ ___  _ __ ___   ___  | |_ ___   | |_| |__   ___ 
+         \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \  | __| '_ \ / _ \\
+          \ V  V /  __/ | (_| (_) | | | | | |  __/ | || (_) | | |_| | | |  __/
+           \_/\_/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/   \__|_| |_|\___|
+           / \   __| |_   _____ _ __ | |_ _   _ _ __ ___                      
+          / _ \ / _` \ \ / / _ \ '_ \| __| | | | '__/ _ \                     
+         / ___ \ (_| |\ V /  __/ | | | |_| |_| | | |  __/                     
+        /_/   \_\__,_| \_/ \___|_| |_|\__|\__,_|_|  \___|                     
+         / ___| __ _ _ __ ___   ___| |                                        
+        | |  _ / _` | '_ ` _ \ / _ \ |                                        
+        | |_| | (_| | | | | | |  __/_|                                        
+         \____|\__,_|_| |_| |_|\___(_)                                        
+        """)
+        print("*" * 50)
+        print("\nPress 'P' to start the game, 'D' for game description, or 'Q' to quit.")
+
+        choice = input().lower()
+        if choice == "p":
+            break
+        elif choice == "d":
+            description_page()
+        elif choice == "q":
+            exit()
+        else:
+            print("Invalid choice. Press 'P' to start the game, 'D' for game description, or 'Q' to quit.")
 
 def description_page():
+    os.system('cls' if os.name == 'nt' else 'clear')
     print("\n" + "=" * 50)
     print("Welcome to the Text-Based Adventure Game!")
     time.sleep(1)
@@ -9,5 +41,10 @@ def description_page():
     print("\nIn this adventure, you find yourself in a main hall with three doors: left, center, and right.")
     time.sleep(1)
     print("Your goal is to explore the rooms, find a passcode, and make choices to complete the adventure.")
+    chose=input("Type 'B' to go back to main page: ")
+    if chose=='B'.lower():
+        welcome_page()
+    else:
+        print("Invalid input")
 
-description_page()
+welcome_page()
