df89aaa3111c2ea361a5a123be5c44f96dd5813b
teman67 <>
2023-10-08 10:04:36 +0000

    add description_page function

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import time
+
+
+def description_page():
+    print("\n" + "=" * 50)
+    print("Welcome to the Text-Based Adventure Game!")
+    time.sleep(1)
+    print("=" * 50)
+    print("\nIn this adventure, you find yourself in a main hall with three doors: left, center, and right.")
+    time.sleep(1)
+    print("Your goal is to explore the rooms, find a passcode, and make choices to complete the adventure.")
+
+description_page()
